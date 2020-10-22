a7073fe9db9d83c59876c5810a47356d7e4fc73d
Hirokazu Uzu <himeatball@gmail.com>
2020-10-22 13:04:03 +0900

    Update Dangerfile.df.kts

--- Dangerfile.df.kts
+++ Dangerfile.df.kts
@@ -1,7 +1,5 @@
 @file:DependsOn("co.uzzu.danger.plugins:checkstyle:0.2.0")
-@file:DependsOn("co.uzzu.strikts:strikts:0.2.0")
 
-import co.uzzu.strikts.*
 import co.uzzu.danger.plugins.checkstyle.*
 import systems.danger.kotlin.Danger
 import systems.danger.kotlin.register
@@ -9,19 +7,6 @@ import systems.danger.kotlin.register
 register plugin CheckStyle
 
 val d = Danger(args)
-val isGitHubActionsBuild = System.getenv("GITHUB_WORKSPACE")?.run { true } ?: false
-
-if (isGitHubActionsBuild) {
-    "**/build/reports/ktlint/ktlint*Check.xml".glob(".")
-        .map { it.toFile() }
-        .filter { !it.isDirectory }
-        .forEach {
-            println(it.absolutePath)
-        }
-    println("runner workspace: ${System.getenv()["RUNNER_WORKSPACE"]}")
-    println("github workspace: ${System.getenv()["GITHUB_WORKSPACE"]}")
-}
-
 CheckStyle.severities = listOf(Severity.IGNORE, Severity.INFO, Severity.WARNING, Severity.ERROR)
 CheckStyle.reporter = Inline
 CheckStyle.report("glob:**/build/reports/ktlint/ktlint*Check.xml")
